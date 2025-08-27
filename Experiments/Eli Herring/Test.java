ecbd946f34d6c276d215494dc8c7a697b4aa8551
Elijah Herring <eliherrr@iastate.edu>
2025-08-27 16:38:13 -0500

    Test

---
+++ Experiments/Eli Herring/Test.java
@@ -0,0 +1,12 @@
+
+
+public class Test {
+    
+    
+    int x = 3;
+    int y = 3;
+    public static void main(String args[]){
+        Test test = new Test();
+        System.out.println(test.x + test.y);
+    }
+}
