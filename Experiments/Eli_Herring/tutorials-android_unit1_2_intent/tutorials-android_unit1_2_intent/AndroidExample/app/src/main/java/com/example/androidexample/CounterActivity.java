f3f0a858dc5d01863db63a8e65a4d8e631fdfe01
Elijah Herring <eliherrr@iastate.edu>
2025-09-10 13:16:31 -0500

    worked on experiment 2

--- Experiments/Eli_Herring/tutorials-android_unit1_2_intent/tutorials-android_unit1_2_intent/AndroidExample/app/src/main/java/com/example/androidexample/CounterActivity.java
+++ Experiments/Eli_Herring/tutorials-android_unit1_2_intent/tutorials-android_unit1_2_intent/AndroidExample/app/src/main/java/com/example/androidexample/CounterActivity.java
@@ -11,13 +11,15 @@ import android.widget.TextView;
 public class CounterActivity extends AppCompatActivity {
 
     private TextView numberTxt; // define number textview variable
+
+    private TextView errorMessage;
     private Button increaseBtn; // define increase button variable
     private Button decreaseBtn; // define decrease button variable
 
     private Button multBtn;
     private Button backBtn;     // define back button variable
 
-
+    private Button divBtn;
     private int counter = 0;    // counter variable
 
     @Override
@@ -27,16 +29,23 @@ public class CounterActivity extends AppCompatActivity {
 
         /* initialize UI elements */
         numberTxt = findViewById(R.id.number);
+        errorMessage = findViewById(R.id.error_msg);
         increaseBtn = findViewById(R.id.counter_increase_btn);
         decreaseBtn = findViewById(R.id.counter_decrease_btn);
         multBtn = findViewById(R.id.counter_mult_btn);
         backBtn = findViewById(R.id.counter_back_btn);
+        divBtn = findViewById(R.id.counter_div_btn);
 
         /* when increase btn is pressed, counter++, reset number textview */
         increaseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberTxt.setText(String.valueOf(++counter));
+                if(counter + 1 >= 10000){
+                    errorMessage.setText("Number is too large");
+                } else {
+                    numberTxt.setText(String.valueOf(++counter));
+                    errorMessage.setText("");
+                }
             }
         });
 
@@ -44,15 +53,40 @@ public class CounterActivity extends AppCompatActivity {
         decreaseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numberTxt.setText(String.valueOf(--counter));
+                if(counter - 1 <= -10000){
+                    errorMessage.setText("Number is too small");
+                } else {
+                    numberTxt.setText(String.valueOf(--counter));
+                    errorMessage.setText("");
+                }
             }
         });
 
         multBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                counter = counter * 2;
-                numberTxt.setText(String.valueOf(counter));
+                if(counter * 2 >= 10000){
+                    errorMessage.setText("Number is too large");
+                } else if(counter * 2 <= -10000){
+                    errorMessage.setText("Number is too small");
+                } else {
+                    counter = counter * 2;
+                    numberTxt.setText(String.valueOf(counter));
+                    errorMessage.setText("");
+                }
+            }
+        });
+
+        divBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(counter % 2 == 0){
+                    counter = counter / 2;
+                    numberTxt.setText(String.valueOf(counter));
+                }else{
+                    errorMessage.setText("Cannot divide an odd number by 2");
+                }
+
             }
         });
 
