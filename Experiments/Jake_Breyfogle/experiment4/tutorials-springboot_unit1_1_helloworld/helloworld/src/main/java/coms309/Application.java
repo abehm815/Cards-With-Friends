fe5a158c6914fd9e9d7ceb265d78332a4c0ff4d2
Jake Breyfogle <jakeb2@iastate.edu>
2025-09-11 13:08:59 -0500

    Pushing experiment 4 where I worked on creating a deck, card, and player class with shuffling and dealing.

---
+++ Experiments/Jake_Breyfogle/experiment4/tutorials-springboot_unit1_1_helloworld/helloworld/src/main/java/coms309/Application.java
@@ -0,0 +1,25 @@
+package coms309;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Deal test application
+ * 
+ * @author Jake Breyfogle
+ */
+
+@SpringBootApplication
+public class Application {
+	
+    public static void main(String[] args) throws Exception {
+        MyDeck testDeck = new MyDeck();
+        Player player1 = new Player("Jake");
+        testDeck.shuffle();
+        for(int i = 0; i < 5; i++) {
+            player1.addCard(testDeck.dealCard());
+        }
+        player1.showHand();
+    }
+
+}
