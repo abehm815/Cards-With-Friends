2a3d70ae9f7cc345fb2768489de5b0f726b4a697
Elijah Herring <eliherrr@iastate.edu>
2025-09-11 13:25:09 -0500

    working on third experiment

--- Experiments/Eli_Herring/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/LoginActivity.java
+++ Experiments/Eli_Herring/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
@@ -19,12 +20,21 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);            // link to Login activity XML
-
         /* initialize UI elements */
         usernameEditText = findViewById(R.id.login_username_edt);
         passwordEditText = findViewById(R.id.login_password_edt);
         loginButton = findViewById(R.id.login_login_btn);    // link to login button in the Login activity XML
         signupButton = findViewById(R.id.login_signup_btn);  // link to signup button in the Login activity XML
+
+
+        String[][] whiteList = new String[][] {
+                {"Tom", "1234"},
+                {"Bob", "5678"},
+                {"Mike", "3333"}
+        };
+
+
+
 
         /* click listener on login button pressed */
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -35,11 +45,18 @@ public class LoginActivity extends AppCompatActivity {
                 String username = usernameEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
 
-                /* when login button is pressed, use intent to switch to Login Activity */
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                intent.putExtra("USERNAME", username);  // key-value to pass to the MainActivity
-                intent.putExtra("PASSWORD", password);  // key-value to pass to the MainActivity
-                startActivity(intent);  // go to MainActivity with the key-value data
+                Intent intent;// go to MainActivity with the key-value data
+                if(username.equals("admin") && password.equals("pass")){
+                    intent = new Intent(LoginActivity.this, AdminActivity.class);
+                    intent.putExtra("PASSWORD", password);
+                } else {
+
+                    /* when login button is pressed, use intent to switch to Login Activity */
+                    intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra("USERNAME", username);  // key-value to pass to the MainActivity
+                    intent.putExtra("PASSWORD", password);  // key-value to pass to the MainActivity
+                }
+                startActivity(intent);
             }
         });
 
