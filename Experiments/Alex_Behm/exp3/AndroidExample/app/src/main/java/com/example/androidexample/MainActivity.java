c161181387b10e92879247ba9a5edc89b0461d61
Alex Behm <abehm815@iastate.edu>
2025-09-12 14:40:13 -0500

    Linked the signup page back to the main activity after signup, added in functionality for logging out (Changes UI)

--- Experiments/Alex_Behm/exp3/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/Alex_Behm/exp3/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.androidexample;
 
+import static android.view.View.VISIBLE;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,43 +10,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-/*
-
-1. To run this project, open the directory "Android Example", otherwise it may not recognize the file structure properly
-
-2. Ensure you are using a compatible version of gradle, to do so you need to check 2 files.
-
-    AndroidExample/Gradle Scripts/build.gradle
-    Here, you will have this block of code. Ensure it is set to a compatible version,
-    in this case 8.12.2 should be sufficient:
-        plugins {
-            id 'com.android.application' version '8.12.2' apply false
-        }
-
-    Gradle Scripts/gradle-wrapper.properties
-
-3. This file is what actually determines the Gradle version used, 8.13 should be sufficient.
-    "distributionUrl=https\://services.gradle.org/distributions/gradle-8.13-bin.zip" ---Edit the version if needed
-
-4. You might be instructed by the plugin manager to upgrade plugins, accept it and you may execute the default selected options.
-
-5. Press "Sync project with gradle files" located at the top right of Android Studio,
-   once this is complete you will be able to run the app
-
-   This version is compatible with both JDK 17 and 21. The Java version you want to use can be
-   altered in Android Studio->Settings->Build, Execution, Deployment->Build Tools->Gradle
-
- */
-
-
 public class MainActivity extends AppCompatActivity {
 
     private TextView messageText;   // define message textview variable
     private TextView usernameText;  // define username textview variable
     private Button loginButton;     // define login button variable
     private Button signupButton;    // define signup button variable
+
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,17 +29,20 @@ public class MainActivity extends AppCompatActivity {
         usernameText = findViewById(R.id.main_username_txt);// link to username textview in the Main activity XML
         loginButton = findViewById(R.id.main_login_btn);    // link to login button in the Main activity XML
         signupButton = findViewById(R.id.main_signup_btn);  // link to signup button in the Main activity XML
+        logoutButton = findViewById(R.id.main_logout_btn);
 
         /* extract data passed into this activity from another activity */
         Bundle extras = getIntent().getExtras();
         if(extras == null) {
             messageText.setText("Euchre");
-            usernameText.setVisibility(View.INVISIBLE);             // set username text invisible initially
+            usernameText.setVisibility(View.INVISIBLE);// set username text invisible initially
+            logoutButton.setVisibility(View.GONE); //Set logout text invisible initially
         } else {
             messageText.setText("Welcome");
             usernameText.setText(extras.getString("USERNAME")); // this will come from LoginActivity
             loginButton.setVisibility(View.INVISIBLE);              // set login button invisible
             signupButton.setVisibility(View.INVISIBLE);             // set signup button invisible
+            logoutButton.setVisibility(VISIBLE);
         }
 
         /* click listener on login button pressed */
@@ -90,5 +66,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        /* click listener on logout button pressed */
+        View.OnClickListener logoutButtonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Reset back to logged out state
+                messageText.setText("Euchre");
+                usernameText.setText("");
+                usernameText.setVisibility(View.INVISIBLE);
+                loginButton.setVisibility(View.VISIBLE);
+                signupButton.setVisibility(View.VISIBLE);
+                logoutButton.setVisibility(View.GONE);
+            }
+        };
+
+        logoutButton.setOnClickListener(logoutButtonListener);
     }
 }
