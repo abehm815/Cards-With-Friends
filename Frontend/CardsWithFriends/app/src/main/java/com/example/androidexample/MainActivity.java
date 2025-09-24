de0223d92d3576a9cc6c6270885f9d74c3d57ae5
Alex Behm <abehm815@iastate.edu>
2025-09-24 17:53:25 -0500

    Working on profile page

--- Frontend/CardsWithFriends/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Frontend/CardsWithFriends/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.androidexample;
 
-import static android.view.View.VISIBLE;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
