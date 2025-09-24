b035b6a1b10028e3e0a6cb7779a632fa406cfc65
Alex Behm <abehm815@iastate.edu>
2025-09-24 14:04:04 -0500

    Fixing gradle files

---
+++ Frontend/CardsWithFriends/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.androidexample;
+
+import static android.view.View.VISIBLE;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;     // define login button variable
+    private Button signupButton;    // define signup button variable
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);             // link to Main activity XML
+
+        loginButton = findViewById(R.id.main_login_btn);    // link to login button in the Main activity XML
+        signupButton = findViewById(R.id.main_signup_btn);  // link to signup button in the Main activity XML
+
+        /* click listener on login button pressed */
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when login button is pressed, use intent to switch to Login Activity */
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        /* click listener on signup button pressed */
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when signup button is pressed, use intent to switch to Signup Activity */
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
