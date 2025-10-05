6a781b69f07047cccbc8681eeb6eaf3c38671902
Alex Behm <abehm815@iastate.edu>
2025-10-05 14:49:49 -0500

    Linked up the signup page to the backend

--- Frontend/CardsWithFriends/app/src/main/java/com/example/androidexample/SignupActivity.java
+++ Frontend/CardsWithFriends/app/src/main/java/com/example/androidexample/SignupActivity.java
@@ -9,6 +9,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+
 public class SignupActivity extends AppCompatActivity {
 
     private EditText usernameEditText;
@@ -17,49 +25,83 @@ public class SignupActivity extends AppCompatActivity {
     private Button signupButton;
     private Button backButton;
 
-    private String username;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        /* initialize UI elements */
+        // Initialize UI elements
         usernameEditText = findViewById(R.id.signup_username_edt);
         passwordEditText = findViewById(R.id.signup_password_edt);
         confirmEditText = findViewById(R.id.signup_confirm_edt);
         signupButton = findViewById(R.id.signup_signup_btn);
         backButton = findViewById(R.id.signup_back_btn);
 
-        /* click listener on signup button pressed */
+        // Handle signup button
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                /* grab strings from user inputs */
-                String username = usernameEditText.getText().toString();
+                String username = usernameEditText.getText().toString().trim();
                 String password = passwordEditText.getText().toString();
                 String confirm = confirmEditText.getText().toString();
 
-                if (password.equals(confirm)){
-                    Intent intent = new Intent(SignupActivity.this, HomeActivity.class);
-                    intent.putExtra("USERNAME", username);
-                    // TODO: Create a PUT request to backend to create account
-                    startActivity(intent);
-                }
-                else {
-                    Toast.makeText(getApplicationContext(), "Passwords do not match", Toast.LENGTH_LONG).show();
+                if (password.equals(confirm)) {
+                    sendSignup(username, password);
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Passwords do not match", Toast.LENGTH_LONG).show();
                 }
             }
         });
 
+        // Handle back button
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SignupActivity.this, MainActivity.class);
-                intent.putExtra("USERNAME", username);
                 startActivity(intent);
             }
         });
+    }
+
+    // Sends signup info to backend
+    private void sendSignup(final String username, final String password) {
+        JSONObject body = new JSONObject();
+        try {
+            body.put("username", username);
+            body.put("password", password);
+        } catch (JSONException e) {
+            Toast.makeText(getApplicationContext(),
+                    "Error Creating Request", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String url = "http://coms-3090-006.class.las.iastate.edu:8080/AppUser";
+
+        JsonObjectRequest postRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                body,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(getApplicationContext(),
+                                "Signup successful!", Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(SignupActivity.this, HomeActivity.class);
+                        intent.putExtra("USERNAME", username);
+                        startActivity(intent);
+                        finish();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Signup Failed", Toast.LENGTH_LONG).show();
+                    }
+                }
+        ) {};
+
+        VolleySingleton.getInstance(SignupActivity.this).addToRequestQueue(postRequest);
     }
 }
