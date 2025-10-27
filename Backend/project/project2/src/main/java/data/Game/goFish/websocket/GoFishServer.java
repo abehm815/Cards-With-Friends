8e0ccce650280716caa314d17507e08483188f85
Jake Breyfogle <jakeb2@iastate.edu>
2025-10-27 16:59:10 -0500

    fixed error with go fish websocket

--- Backend/project/project2/src/main/java/data/Game/goFish/websocket/GoFishServer.java
+++ Backend/project/project2/src/main/java/data/Game/goFish/websocket/GoFishServer.java
@@ -5,6 +5,7 @@ import data.Game.goFish.GoFishGame;
 import data.Game.goFish.GoFishPlayer;
 import jakarta.websocket.*;
 import jakarta.websocket.server.ServerEndpoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
+@Component
 @ServerEndpoint("/gofish")
 public class GoFishServer {
     public static final Set<Session> sessions = new CopyOnWriteArraySet<>();
