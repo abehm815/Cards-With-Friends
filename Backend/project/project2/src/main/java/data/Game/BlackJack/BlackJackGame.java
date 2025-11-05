e6618ea6b3845d748c2d61e54037b0c19419a4fb
Colten Stevens <coltenjs@iastate.edu>
2025-11-04 22:39:48 -0600

    still bugging

--- Backend/project/project2/src/main/java/data/Game/BlackJack/BlackJackGame.java
+++ Backend/project/project2/src/main/java/data/Game/BlackJack/BlackJackGame.java
@@ -106,6 +106,7 @@ public class BlackJackGame {
                     user.setHasStood(true);
                 }
             }
+            currentPlayerIndex--;
             advanceTurn();
             notifyCurrentPlayerTurn();
         }
